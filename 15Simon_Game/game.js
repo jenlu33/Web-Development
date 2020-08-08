2530b6d963d41bdb5a47b0751ad773808326b23c
jenlu33 <nejul33@gmail.com>
2020-08-08 13:27:06 -0400

    Finish section 15

--- 15Simon_Game/game.js
+++ 15Simon_Game/game.js
@@ -1,19 +1,24 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
-const gamePattern = [];
-const userClickedPattern = [];
+let gamePattern = [];
+let userClickedPattern = [];
 
 let level = 0;
-let start = false;
+let started = false;
 
 // if game has not started yet, press any key to start. Else nothing happens
-// if (!start) {
-//   $("body").keydown(() => nextSequence())
-// } else {
-//   console.log("what");
-// }
+$("body").keydown(() => {
+  if (!started) {
+    setTimeout(() => {
+      nextSequence();
+    }, 200)
+    
+    $("#level-title").text(`Level ${level}`)
+    started = true;
+  }
+});
 
 function nextSequence() {
-  start = true;
+  started = true;
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
@@ -47,6 +52,44 @@ function animatePress(currentColor) {
     $(`#${currentColor}`).removeClass("pressed")
   }, 100);
 };
+
+$("div[type='button']").click(() => {
+  checkAnswer(userClickedPattern.length - 1);
+})
+
+function checkAnswer(currentLevel) {
+  // checks to see if current click matches with gamePattern
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+      console.log('success');
+    } else { // game over
+      let wrongSound = new Audio('sounds/wrong.mp3');
+      wrongSound.play();
+
+      $("body").addClass("game-over");
+      setTimeout(() => {
+        $("body").removeClass("game-over")
+      }, 200);
+
+      $("#level-title").text("Game Over, Press Any Key to Restart");
+
+      restartGame();
+    }
+
+    // if everything matches and currentLevel === actual current level then move on to next level and reset clicks
+    if (currentLevel === gamePattern.length - 1) {
+      setTimeout(() => {
+        nextSequence()
+      }, 1000);
+      userClickedPattern = [];
+    }
+};
+
+function restartGame() {
+  started = false;
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+}
 
 
 

b4392f3f0e7ad171aa716b2973dea37a8f89fd96
jenlu33 <nejul33@gmail.com>
2020-08-11 17:14:23 -0400

    Finish calculator challenge

--- 19Calculator/calculator.js
+++ 19Calculator/calculator.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+// parse data that comes from an HTML form
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -8,4 +13,12 @@ app.get("/", (req, res) => {
 // __dirname gives you the file path of the current file 
 // no matter where it's hosted (on somebody else's computer, in the cloud, etc)
 
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let result = num1 + num2;
+  res.send(`The result of the calculation is ${result}`)
+})
+
 app.listen(3000, () => console.log("server started on port 3000"));
+ 
